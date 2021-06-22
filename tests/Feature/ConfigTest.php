4c5f91ba97b51883b058bd44120a91235d4084b8
Stephen Neal <stephen.neal14@gmail.com>
2021-06-22 12:02:50 -0400

    MAKE `ConfigTest` feature test for testing tracking event values
    MAKE `MockTrackingEvent` event asset.

---
+++ tests/Feature/ConfigTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sfneal\CrudModelActions\Tests\Feature;
+
+use Illuminate\Support\Facades\Config;
+use Sfneal\CrudModelActions\Tests\Assets\Events\MockTrackingEvent;
+use Sfneal\CrudModelActions\Tests\TestCase;
+use Sfneal\Events\Event;
+
+class ConfigTest extends TestCase
+{
+    /** @test */
+    public function event_default()
+    {
+        $output = config('crud-model-action.event');
+
+        $this->assertNull($output);
+    }
+
+    /** @test */
+    public function event_with_setting()
+    {
+        $key = 'crud-model-action.event';
+        $expected = MockTrackingEvent::class;
+        Config::set($key, $expected);
+        $output = config($key);
+
+        $this->assertIsString($output);
+        $this->assertTrue(class_exists($output));
+        $this->assertEquals($expected, $output);
+    }
+}
