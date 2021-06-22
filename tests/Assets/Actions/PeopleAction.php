eea8744310f9843319d819a2daf30637b82c5caa
Stephen Neal <stephen.neal14@gmail.com>
2021-06-22 12:04:05 -0400

    ADD use of a tracking event in mock `PeopleAction`

--- tests/Assets/Actions/PeopleAction.php
+++ tests/Assets/Actions/PeopleAction.php
@@ -5,6 +5,7 @@ namespace Sfneal\CrudModelActions\Tests\Assets\Actions;
 
 
 use Sfneal\CrudModelActions\CrudModelAction;
+use Sfneal\CrudModelActions\Tests\Assets\Events\MockTrackingEvent;
 use Sfneal\Testing\Models\People;
 
 abstract class PeopleAction extends CrudModelAction
@@ -19,5 +20,10 @@ abstract class PeopleAction extends CrudModelAction
      *
      * @var People
      */
-    protected $modelClass;
+    protected $modelClass = People::class;
+
+    /**
+     * @var MockTrackingEvent Model Tracking Event to fire
+     */
+    protected $trackingEvent = MockTrackingEvent::class;
 }
