1a66f8add3c7a0696931636fa3b4cc541909aff6
Stephen Neal <stephen.neal14@gmail.com>
2021-06-22 12:29:52 -0400

    ADD 'model_can_be_created()' test method to `CrudModelActionCreateTest`

--- tests/Unit/CrudModelActionCreateTest.php
+++ tests/Unit/CrudModelActionCreateTest.php
@@ -4,9 +4,52 @@
 namespace Sfneal\CrudModelActions\Tests\Unit;
 
 
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Sfneal\CrudModelActions\Tests\Assets\Actions\CreatePeopleAction;
+use Sfneal\CrudModelActions\Tests\Assets\Requests\PostRequest;
 use Sfneal\CrudModelActions\Tests\TestCase;
+use Sfneal\Testing\Models\People;
+use Sfneal\Testing\Utils\Interfaces\RequestCreator;
 
-class CrudModelActionCreateTest extends TestCase
+class CrudModelActionCreateTest extends TestCase implements RequestCreator
 {
+    use PostRequest;
 
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function model_can_be_created()
+    {
+        $data = [
+            'data' => [
+                'name_first' => 'Victor',
+                'name_last' => 'Hedman',
+                'email' => 'vhedman@example.com',
+                'age' => 29,
+            ]
+        ];
+        $request = $this->createRequest([], $data);
+
+        $response = (new CreatePeopleAction($request))->execute();
+
+        $query = People::query()
+            ->where('name_first', '=', 'Victor')
+            ->where('name_last', '=', 'Hedman')
+            ->where('email', '=', 'vhedman@example.com')
+            ->where('age', '=', 29);
+
+        $this->assertSame(1, $query->count());
+
+        $model = $query->get()->first();
+
+        $this->assertTrue($query->exists());
+        $this->assertInstanceOf(Model::class, $model);
+        $this->assertInstanceOf(People::class, $model);
+
+        foreach ($data['data'] as $key => $attribute) {
+            $this->assertEquals($attribute, $model->{$key});
+        }
+    }
 }
